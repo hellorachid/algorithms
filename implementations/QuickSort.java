b27141ccb2d69391635419f65fb2bc9d594a883b
Rachid HAKMI <rachid.hakmi@gmail.com>
2015-09-02 21:33:33 +0100

    add Quick sort wikipedia link

--- implementations/QuickSort.java
+++ implementations/QuickSort.java
@@ -1,3 +1,10 @@
+/*
+*	Queue is a linear structure which follows a particular order in which the operations are performed. 
+*	The order is First In First Out (FIFO).  A good example of queue is any queue of consumers for 
+*	a resource where the consumer that came first is served first.
+*	
+*	more info : https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+*/
 public class QuickSort {
 
 	public static void main(String args[]) {
