daceb6d2f54e9a7cfd8f248b165d8c215d547d25
Rachid HAKMI <rachid.hakmi@gmail.com>
2015-09-03 19:38:35 +0100

    Count digit in a range form 0 to a given number

---
+++ implementations/CountDigitInRange.java
@@ -0,0 +1,37 @@
+public class CountDigitInRange {
+
+	public static void main(String args[]) {
+
+		if(args.length != 2) {
+			System.out.println("Help :");
+			System.out.println("	java CountDigitInRange num digit");
+			System.exit(-1);
+		}
+		String numString = args[0];
+		String digitString = args[1];
+		try {
+			int num = Integer.parseInt(numString);
+			int digit = Integer.parseInt(digitString);
+			int result = countDigitInRange(num, digit);
+			System.out.printf("number of %d in 0..%d is %d \n", digit, num, result);
+		} catch(NumberFormatException nfe) {
+			System.out.println("num or digit is not a number");
+		}
+
+	}
+
+	public static int countDigitInRange(int max, int digit) {
+		int count = 0;
+		for(int i = 0; i <= max; i++) {
+			int n = i;
+			while(n > 0) {
+				if(n % 10 == digit) {
+					count++;
+				}
+				n = n / 10;
+			}
+		}
+		return count;
+	}
+
+}
