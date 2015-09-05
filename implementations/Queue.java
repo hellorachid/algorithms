b2c37d15354c2d257f5e61a17138ac4c352b8156
Rachid HAKMI <rachid.hakmi@gmail.com>
2015-09-05 21:31:12 +0100

    Queue implementation

---
+++ implementations/Queue.java
@@ -0,0 +1,64 @@
+
+public class Queue {
+	private Node front;
+	private Node rear;
+	
+	public void enqueue(int a) {
+		Node node = new Node();
+		node.data = a;
+		if (isEmpty()) {
+			this.front = this.rear = node;
+		} else {
+			this.rear.next = node;
+			this.rear = node;
+		}
+	}
+	
+	public Node dequeue() {
+		if (isEmpty()) {
+			return null;
+		} else if (this.front == rear) {
+			Node temp = this.front;
+			this.front = rear = null;	
+			return temp;
+		} else {
+			Node temp;
+			temp = this.front;
+			this.front = front.next;
+			return temp;
+		}
+	}
+	
+	public boolean isEmpty() {
+		return (this.front == this.rear) && (this.front == null);
+	}
+
+	public Node front() {
+		return front;
+	}
+
+
+	public static void main(String args[]) {
+		Queue q = new Queue();
+		System.out.println(q.isEmpty());
+		System.out.println(q.dequeue());
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(4);
+
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+		System.out.println(q.dequeue());
+	}
+}
+
+class Node {
+	public int data;
+	public Node next;
+
+	public String toString() {
+		return String.valueOf(this.data);
+	} 
+}
