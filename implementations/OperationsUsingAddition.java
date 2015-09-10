d7d7794e843d3da16b49b7083e354ae3116ffa0d
Rachid HAKMI <rachid.hakmi@gmail.com>
2015-09-10 22:58:27 +0100

    Explain how arithmetic operations using just addition works

--- implementations/OperationsUsingAddition.java
+++ implementations/OperationsUsingAddition.java
@@ -1,10 +1,34 @@
+/*
+*	How it works :
+*		substracttion :  
+*			we can use addition to do substraction using two's complement, 
+*			a - b  ==> a + (two's complement of b) ==> a + ~b
+*			(we use two's complement to get the negative of a number)
+*
+*		multiplication :
+*			we add number a  b time
+*			a = 2, b = 3 => 2 + 2 + 2
+*
+*		division :
+*			we substract b from a until we get a number smaller than b and we count the number of times
+*			a = 8, b = 3
+*           Steps :
+*				8 - 3 = 5  | index = 1
+*				5 - 3 = 2  | index = 2
+*				2 < 3 so we stop 
+*				we return 2
+*
+*
+*/
+
 public class OperationsUsingAddition {
 
 	public static void main(String args[]) {
 		
-		System.out.println(multiply(1, 0));
-		System.out.println(substract(0, 5));
-		System.out.println(devide(5, 0));
+		System.out.printf(" %d * %d = %d \n", 1, 0, multiply(1, 0));
+		System.out.printf(" %d - %d = %d \n", 0, 5, substract(0, 5));
+		System.out.printf(" %d / %d = %d \n", 5, 0, devide(5, 0));
+		System.out.printf(" %d / %d = %d \n", 8, 3, devide(8, 3));
 	}
 
 	public static int multiply(int a, int b) {
@@ -34,6 +58,7 @@ public class OperationsUsingAddition {
 	public static int devide(int a, int b) {
 		int index = 0;
 		if (b == 0) {
+			// -1 means that 
 			return -1;
 		}
 		while (a >= b) {
