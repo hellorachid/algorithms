3e14408a84f1281c65a42ea4fc713ee2b21f9d05
Rachid HAKMI <rachid.hakmi@gmail.com>
2015-09-13 23:42:51 +0100

    explain how replace spaces with %20 in a given string works

--- implementations/ReplaceSpaces.java
+++ implementations/ReplaceSpaces.java
@@ -1,3 +1,25 @@
+/*
+*	Problem :
+*		we have a given string with spaces, we want to change every space with %20
+*		we consider that the string have enough space at the end and the given length
+*		consider just the string 
+*	example :	
+*		String : "Mr John Smith    "
+*							  |
+*							length
+*		length : 13 - 1 because we begin at index 0
+*
+*		
+*	How it works :
+*		first, we consider that we have enough space at the end of the string to fill %20, 
+*		the length of the string not include this spaces
+*		we calculate the last index by the formulat : length of the string + number of spaces * 2
+*		( because %20 is 3 chars, we have already one from the space in the string and 2 from 
+*		the spaces in the string that is not considered :D )
+*		we commence from the string lengh index and we shift the values to the last index
+*		if we find a space we add %20 and we continue
+*/
+
 public class ReplaceSpaces {
 
 	public static void main( String args[] ) {
